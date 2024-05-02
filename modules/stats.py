4fea976fd29a261ed3e41a3ec74bf606ee1acc0f
clerickbarrion <132791511+clerickbarrion@users.noreply.github.com>
2024-05-02 16:49:51 -0400

    Update stats.py

--- modules/stats.py
+++ modules/stats.py
@@ -2,13 +2,13 @@ import csv
 
 # Returns # of deaths per year
 def deaths_by_year():
-    with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+    with open('data/Accidental_Drug_Deaths_Cleansed.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         # Dict containing deaths per year
         deaths_by_year = {year: 0 for year in range(2012, 2023)}
         # Iterates through csv file
         for row in reader:
-            year = int(row['Date'][6:])
+            year = int(row['Date'])
             # Adds a death to respective year in the dict
             if year in deaths_by_year:
                 deaths_by_year[year] += 1
@@ -16,7 +16,7 @@ def deaths_by_year():
 
 # Returns # and % of drugs used in a given year
 def drug_distribution_by_year(year):
-  with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+  with open('data/Accidental_Drug_Deaths_Cleansed.csv') as csvfile:
     # Dict containing drugs and their totals and percentages
     drugs = {
         # Drug: [Deaths, Percentage]
@@ -41,9 +41,9 @@ def drug_distribution_by_year(year):
     reader = csv.DictReader(csvfile)
     for row in reader:
         # Checks if year in file matches given year
-        if int(row['Date'][6:]) == year:
+        if int(row['Date']) == year:
             # Gets cause of death and drugs involved in it
-            drug = row['Cause of Death'].lower()
+            drug = row['Real Cause of Death'].lower()
             # Checks if any known drug is found
             found = False
             # Iterates through known drugs
@@ -63,7 +63,7 @@ def drug_distribution_by_year(year):
 
 # Returns total deaths of each age in a given range
 def death_age_range(lowest, highest):
-    with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+    with open('data/Accidental_Drug_Deaths_Cleansed.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         ages = {str(i): 0 for i in range(lowest, highest+1)}
         for row in reader:
@@ -76,11 +76,11 @@ def death_age_range(lowest, highest):
 
 # Returns death by race in a given year
 def death_by_race(year):
-  with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+  with open('data/Accidental_Drug_Deaths_Cleansed.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-    races = {"White": 0, "Black": 0, "Asian": 0, "Black or African American": 0, "Asian/Indian": 0, "Other": 0}
+    races = {"White": 0, "Black": 0, "Asian - All": 0, "Black or African American": 0, "Unknown": 0, "Other": 0}
     for row in reader:
-      if int(row['Date'][6:]) == year:
+      if int(row['Date']) == year:
         # If the race in the row is found in the dict, adds 1
         try:
           races[row['Race']] += 1
@@ -91,7 +91,7 @@ def death_by_race(year):
 
 # Returns drug usage between male and female of a given drug
 def male_female_drug_usage(drug):
-    with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+    with open('data/Accidental_Drug_Deaths_Cleansed.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         # Dict with totals
         male_female = {
@@ -101,7 +101,7 @@ def male_female_drug_usage(drug):
         }
         for row in reader:
             # Checks if given drug is present in the cause of death
-            if drug.lower() in row['Cause of Death'].lower():
+            if drug.lower() in row['Real Cause of Death'].lower():
                 # Adds to male or female count based on gender
                 if row['Sex'] == 'Male':
                     male_female["Male"] += 1
